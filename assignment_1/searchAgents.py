793b7d979c4d08df8a50a23a5586e491f89bf54f
jmurrah <jacobhmurrah@gmail.com>
2025-02-14 09:30:29 -0600

    q5 work in progress

--- assignment_1/searchAgents.py
+++ assignment_1/searchAgents.py
@@ -344,7 +344,7 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition
 
     def isGoalState(self, state: Any):
         """
